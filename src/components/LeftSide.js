11650d8f6fe2034c1ba83423c3b76c26d85dfd3d
bielberg <sergeev_ms03@mail.ru>
2022-06-13 10:47:40 +0300

    reduced code

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -4,47 +4,20 @@ import { NowTab } from './NowTab';
 import { DetailsTab } from './DetailsTab';
 
 export function LeftSide({dataCity, srcHeart, toggleHeart}){
+    const [value, setValue] = useState(1);
+  
+    function handleValue(e){
+      setValue(e.target.value);
+    }
 
-    const [nowTab, setNowCity] = useState("tabBtn nowTab onClick");
-    const [detailsTab, setDetailsCity] = useState("tabBtn detailsTab");
-    const [forecastTab, setForecastCity] = useState("tabBtn forecastTab");
-    const [nowStyle, setNowStyle] = useState("block");
-    const [detailsStyle, setDetailsStyle] = useState("none");
-    const [forecastStyle, setForecastStyle] = useState("none");
-  
-    function toggleTab(e){
-      setNowCity("tabBtn nowTab");
-      setDetailsCity("tabBtn detailsTab");
-      setForecastCity("tabBtn forecastTab");
-      setNowStyle("none");
-      setDetailsStyle("none");
-      setForecastStyle("none");
-      toggle(e.target.value);
-    }
-    
-    function toggle(value){
-      if(value == 1) {
-        setNowCity(nowTab + " onClick");
-        setNowStyle("block");
-      }
-      if(value == 2) {
-        setDetailsCity(detailsTab + " onClick");
-        setDetailsStyle("block");
-      }
-      if(value == 3) {
-        setForecastCity(forecastTab + " onClick");
-        setForecastStyle("block");
-      }
-    }
-  
     return (
       <div className="tabs">
-        <NowTab dataCity={dataCity} style={nowStyle} srcHeart={srcHeart} toggleHeart={toggleHeart}/>
-        <DetailsTab dataCity={dataCity} style={detailsStyle}/>
-        <ForecastTab dataCity={dataCity}  style={forecastStyle}/>
-        <button className={nowTab} onClick={toggleTab} value={1}>Now</button>
-        <button className={detailsTab} onClick={toggleTab} value={2}>Details</button>
-        <button className={forecastTab} onClick={toggleTab} value={3}>Forecast</button>
+        <NowTab dataCity={dataCity} style={value === "1" ? "block" : "none"} srcHeart={srcHeart} toggleHeart={toggleHeart}/>
+        <DetailsTab dataCity={dataCity} style={value === "2" ? "block" : "none"}/>
+        <ForecastTab dataCity={dataCity}  style={value === "3" ? "block" : "none"}/>
+        <button className={value === "1" ? "tabBtn nowTab onClick" : "tabBtn nowTab"} onClick={handleValue} value={1}>Now</button>
+        <button className={value === "2" ? "tabBtn detailsTab onClick" : "tabBtn detailsTab"} onClick={handleValue} value={2}>Details</button>
+        <button className={value === "3" ? "tabBtn forecastTab onClick" : "tabBtn forecastTab"} onClick={handleValue} value={3}>Forecast</button>
       </div>
     );
   }
